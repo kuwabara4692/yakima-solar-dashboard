f3e3e5a8a897c3cdfd1591c792ccbc5573837d28
ckohler4692 <ckohler4692@gmail.com>
2025-09-17 20:55:28 -0700

    Update app.py
    port = int(os.environ.get("PORT", 8050))
    app.run(host="0.0.0.0", port=port, debug=True)

--- app.py
+++ app.py
@@ -116,4 +116,5 @@ def update_temperature_graph(contents, filename):
         return html.P(f"Error processing file: {e}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 8050))
+app.run(host="0.0.0.0", port=port, debug=True)
